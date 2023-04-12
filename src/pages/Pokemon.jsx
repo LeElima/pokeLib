d7fe6e6f2505709146df917c94f7757b7686263f
Leandro de Lima <leandrolima@cobrape.com.br>
2023-04-12 09:05:42 -0300

    Inicio desenvolvimento tab

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -7,6 +7,16 @@ import './Pokemon.css';
 const Pokemon = () =>{
     const { id } = useParams();
     const[infos, setInfos] = useState(null);
+    const[nav, setNav] = useState('Stats')
+     function openAbs(cityName){
+        var i;
+        var x = document.getElementsByClassName("option");
+        console.log(x)
+        for (i = 0; i < x.length; i++) {
+        //   x[i].style.display = "none";  
+        }
+        // document.getElementById(cityName).style.display = "block";  
+      }
     const getInfos = async (url) => {
         const res = await fetch(url);
         const data = await res.json();
@@ -32,9 +42,6 @@ const Pokemon = () =>{
                             <div className="nomePokemon">
                                 <h3>{infos.name}</h3>
                                 
-                                {/* {Object.values(infos.types).map((x)=>{
-                                    <p key={x.type}>{x.type}</p>
-                                })} */}
                             </div>
                             <div className="type">
                                     {infos.types.map((x)=>{
@@ -66,8 +73,19 @@ const Pokemon = () =>{
                             </div>
                         </div>
                     </div>
-                    
-                    
+                    <div className="aba-navegacao">
+                        <button onClick={openAbs('Stats')}>Stats</button>
+                        <button onClick={openAbs('Moves')}>Moves</button>
+                    </div>
+                    <div id="Stats" className="option">
+                    <h2>London</h2>
+                    <p>London is the capital city of England.</p>
+                    </div>
+
+                    <div id="Moves" className="option" style={{display: "none"}}>
+                    <h2>Paris</h2>
+                    <p>Paris is the capital of France.</p> 
+                    </div>
                     
                     <div className="moves">
                         <div className="title">Moves</div>
